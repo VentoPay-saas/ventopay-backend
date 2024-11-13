f0f29e19670956e9d58551d2cc2090df0d5c069f
saudkhan <khannihar921@gmail.com>
2024-11-13 15:48:00 +0500

    create the order api's full crud

---
+++ routes/OrderRoutes.js
@@ -0,0 +1,18 @@
+import express from "express";
+import {
+  createOrder,
+  deleteOrder,
+  getAllOrders,
+  getOrderById,
+  updateOrder,
+} from "../controllers/OrderController.js";
+
+const router = express.Router();
+
+router.get("/orders", getAllOrders);
+router.post("/orders", createOrder);
+router.get("/orders/:_id", getOrderById);
+router.delete("/orders/:_id", deleteOrder);
+router.put("/orders/:_id", updateOrder);
+
+export default router;
